fff5d7eab4e4eee5b895dcdab7f67ba91e8bc8ee
k.papikiyan <kp@gusi-lebedi.ru>
2021-10-31 23:29:32 +0300

    Add main container

---
+++ components/mainContainer.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+export const MainContainer: FC<{}> = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 1280px;
+  height: auto;
+  background: #ffffff;
+  border-radius: 16px;
+  margin: 0 auto;
+  margin-top: 50px;
+  box-sizing: border-box;
+  padding: 80px 80px 80px 80px;
+`;
